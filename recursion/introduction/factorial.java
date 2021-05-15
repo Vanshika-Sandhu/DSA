6d1bb448dd2fe8dc88d8c083130cf8f4e884b9b4
= <=>
2021-05-15 22:19:29 +0530

    factorial

---
+++ recursion/introduction/factorial.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class factorial {
+
+    public static int fact(int n){
+        if(n == 0)
+            return 1;
+        return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int n = scn.nextInt();
+        int f = fact(n);
+        System.out.println("Factorial of " + n + " is : " + f );
+        scn.close();
+    }
+}
