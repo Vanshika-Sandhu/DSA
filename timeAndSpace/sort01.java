c09828d4f11f99142fc6e0846c58e20642d4c969
= <=>
2021-05-28 21:49:33 +0530

    sort 01 linear

---
+++ timeAndSpace/sort01.java
@@ -0,0 +1,69 @@
+// 1. You are given an array(arr) containing only 0's and 1's.
+// 2. You have to sort the given array in increasing order and in linear time.
+
+// Sample Input
+// 5
+// 0
+// 1
+// 0
+// 1
+// 0
+// Sample Output
+// Swapping index 0 and index 0
+// Swapping index 2 and index 1
+// Swapping index 4 and index 2
+// 0
+// 0
+// 0
+// 1
+// 1
+
+
+package timeAndSpace;
+
+import java.util.*;
+
+public class sort01 {
+
+    public static void sort01Func(int[] arr){
+        //write your code here
+
+        int i = 0;
+        int j = 0;
+        while(i<arr.length){
+            if(arr[i] == 0){
+                swap(arr, i, j);
+                i++;
+                j++;
+            }else{
+                i++;
+            }
+        }
+        
+      }
+    
+      // used for swapping ith and jth elements of array
+      public static void swap(int[] arr, int i, int j) {
+        System.out.println("Swapping index " + i + " and index " + j);
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    
+      public static void print(int[] arr){
+        for(int i = 0 ; i < arr.length; i++){
+          System.out.println(arr[i]);
+        }
+      }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ;i < n; i++){
+        arr[i] = scn.nextInt();
+        }
+        sort01Func(arr);
+        print(arr);
+        scn.close();
+    }
+}
