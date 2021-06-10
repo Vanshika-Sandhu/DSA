0e4829a4724150b80a533dd54064ae3840d29671
= <=>
2021-06-10 23:05:07 +0530

    without using arrays

--- dynamicProgrammingAndGreedy/countBinaryStrings.java
+++ dynamicProgrammingAndGreedy/countBinaryStrings.java
@@ -22,9 +22,12 @@ public class countBinaryStrings {
         // meaning of cell 
         // for arr0[i] => count of binary string following the rules, of length i and ending with 0
         // for arr1[i] => count of binary string following the rules, of length i and ending with 1
+
+
+        // using arrays
+        /*
         int[] arr0 = new int[n+1];
         int[] arr1 = new int[n+1];
-        
         arr0[1] = 1;
         arr1[1] = 1;
         for(int i=2; i<n+1; i++){
@@ -33,6 +36,23 @@ public class countBinaryStrings {
             arr1[i] = arr0[i-1]+arr1[i-1];
         }
         System.out.println(arr0[n]+arr1[n]);
+        */
+
+
+        // without using arrays
+        int oldzero = 1;
+        int oldone = 1;
+
+        for(int i=2; i<n+1; i++){
+            int newzero = oldone;
+            int newone = oldone + oldzero;
+
+            oldzero = newzero;
+            oldone = newone;
+        }
+
+        System.out.println(oldzero+oldone);
+
 
         scn.close();
     }
