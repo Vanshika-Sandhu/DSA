975fbfca7264cd610c3b631a8a2ecb269d42fd05
= <=>
2021-07-28 11:58:06 +0530

    intro to linked list and its data members

---
+++ linkedLists/introLinkedLists.java
@@ -0,0 +1,30 @@
+// linked list -> collection of nodes
+// nodes -> contain data and address of the next node
+
+
+package linkedLists;
+import java.util.*;
+
+public class introLinkedLists {
+
+    public static class Node {
+        int data;    
+        Node next;    // this would be addressing the next node. they have the same interface(Node type)
+      }
+    
+      public static class LinkedList {
+        Node head;    // address of first node in ll
+        Node tail;    // address of last node in ll
+        int size;     // size of ll
+    
+        // and the rest functions of linked list
+
+      }
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        scn.close();
+    }
+    
+}
