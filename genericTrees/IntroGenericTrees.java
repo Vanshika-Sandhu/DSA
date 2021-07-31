87c5006547e8a07848495329ea1407aabbde98f7
= <=>
2021-07-31 14:16:51 +0530

    intro to generic trees

---
+++ genericTrees/IntroGenericTrees.java
@@ -0,0 +1,50 @@
+package genericTrees;
+import java.util.*;
+
+public class IntroGenericTrees {
+    private static class Node{
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static void Display(Node node){
+        
+        String str = node.data + " -> ";
+        for(Node child : node.children){
+            str+= child.data + ", ";
+        }
+        str+=".";
+        System.out.println(str);
+
+        for(Node child : node.children){
+            Display(child);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int[] arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+        Node root = null;
+
+        Stack<Node> st = new Stack<>();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == -1){
+                st.pop();
+            }else{
+                Node temp = new Node();
+                temp.data = arr[i];
+                if(st.size()>0){
+                    st.peek().children.add(temp);
+                }else{
+                    root = temp;
+                }
+                st.push(temp);
+            }
+        }
+
+        Display(root);
+
+        scn.close();
+    }
+    
+}
